cb4f3293a559086e8185e73d3e4aa12be17c2f66
kevin-MacBookAir <kevinchevallier@gmail.com>
2015-04-27 23:17:58 -0300

    Initial commit

---
+++ js/app/pageTitle.js
@@ -0,0 +1,12 @@
+define(["jquery"], function () {
+
+		var title = $("h1").text();
+		$(document).prop('title', title);
+		$("#primary_nav_wrap a").each(function(){
+			if( $(this).text() == title ){
+				$(this).addClass('active');
+				return false;
+			}
+		});
+
+});
