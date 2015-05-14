5560adaf0838b8203825e91037558e2856af3c5b
kevin-MacBookAir <kevinchevallier@gmail.com>
2015-05-14 01:22:04 -0300

    Including lodash and high-order function in order to just display enabled menu items

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
 requirejs.config({
     baseUrl: 'js/lib',
     paths: {
-        app: '../app',
-        Q:   '../lib'
+        app:    '../app',
+        Q:      '../lib',
+        lodash: '../lib/lodash'
     }
 });
 
+require(['lodash'], function(_) {
+  console.log('lodash version: ' + _.VERSION);
+  // _.filter
+});
+
 // using Q for Promises
-require(['app/menu', 'app/loadpage', 'q'], function (menu, loadpage, Q) {
-	menu.domenu().then(function () {
+require(['lodash', 'app/menu', 'app/loadpage', 'q'], function (_, menu, loadpage, Q) {
+	menu.domenu().then(function (){
         console.log("domenu successful");
         loadpage.doloadpage();
     }).catch(function (error) {
--- js/app/menu.js
+++ js/app/menu.js
@@ -4,7 +4,16 @@ define(function (require) {
 	var Q = require('q');
 	
 	var domenu = function(){
-		var deferral = Q.defer();
+		// creating console element
+		createElement("div","console");
+		// creating h1 element
+		createElement("h1","result");
+		// creating nav element and appending to body
+		createElement("nav","menuwrapper");
+		
+		var nav = document.getElementById("menuwrapper"),
+			deferral = Q.defer(),
+			request = new XMLHttpRequest();
 
 		function createElement(type, id){
 			var el=document.createElement(type);
@@ -12,48 +21,50 @@ define(function (require) {
 			document.body.appendChild(el);
 		}
 
-		// creating console element
-		createElement("div","console");
-
-		// creating h1 element
-		createElement("h1","result");
-
-		// creating nav element and appending to body
-		createElement("nav","menuwrapper");
-		var nav = document.getElementById("menuwrapper");
-		
 		// json call
-		var request = new XMLHttpRequest();
 		request.open('GET', 'api/menu/menu.json', true);
 
 		request.onload = function() {
 			if (this.status >= 200 && this.status < 400) {
 
-				var ul = document.createElement("ul"),
-					data = JSON.parse(this.response),
-					menu = [],
-					result  = document.getElementById("result");
+				var ul        = document.createElement("ul"),
+					jsonParse = JSON.parse(this.response),
+					menu      = [],
+					result    = document.getElementById("result");
 
-					function build(object){
-						var h = "#";
-						
-						if (object.status == "enabled"){
-							h = object.path;
-						} 
-						menu.push( "<li><a data-status='"+object.status+"' data-route='"+object.route+"' href=# id="+object.path+">"+object.text+"</a>" );
-						if ( object.menu ){
-							menu.push( "<ul>" );
-							object.menu.forEach(function(item){
-								build(item);
-							});
-							menu.push( "</ul>" );
-						}
+				// High-order function
+				function isEnabled(item){
+					if ( _.get(item, 'status') == 'enabled' ){
+						console.log(item.text+': good for menu');
+						return buildMenu(item);
+					} else {
+						console.log(item.text+': no good for menu');
+						return false;
+					}
+				};
 
-						menu.push("</li>");
-					};
+				// building the menu structure
+				function buildMenu(object){
+					var status = _.get(object, 'status'),
+						route  = _.get(object, 'route'),
+						path   = _.get(object, 'path'),
+						text   = _.get(object, 'text');
 
-				data.menu.forEach(function(item){
-					build(item);
+					menu.push( "<li><a data-status='"+status+"' data-route='"+route+"' href=# id="+path+">"+text+"</a>" );
+					// Not in use currently as we just use status.enabled and not .dropdown 
+					// if ( object.menu ){
+					// 	menu.push( "<ul>" );
+					// 	object.menu.forEach(function(item){
+					// 		buildMenu(item);
+					// 	});
+					// 	menu.push( "</ul>" );
+					// }
+					menu.push("</li>");
+				};
+
+				// starting to buidl the menu
+				jsonParse.menu.forEach(function(item){
+					isEnabled( item );
 				});
 
 				// creating inner menu and appending to nav
